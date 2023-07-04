f6f059255b3393cc73566ae3e62423923178d52c
MPA-SE <priscillamomodu@gmail.com>
2023-07-03 18:05:06 -0700

    puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# using puppet to change configuration file
+
+file {'etc/ssh/ssh_config':
+	ensure => present,
+
+content =>"
+
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	passwordAuthentification no
+
+}
